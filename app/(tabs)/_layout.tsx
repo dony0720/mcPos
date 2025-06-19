2090e8a11015bbdac4d250cb6144874e1d02bc2d
Jungseok Shin <jsjb7164@naver.com>
2025-06-20 00:00:32 +0900

    feat : 메뉴 선택 페이지 퍼블리싱 작업

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 import { Tabs } from "expo-router";
-import { Text } from "react-native";
-
+import { View, Text } from "react-native";
 export default function TabLayout() {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false, // 모든 탭의 헤더를 숨김
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "메뉴",
+        }}
+      />
+      <Tabs.Screen
+        name="ledger"
+        options={{
+          title: "시재관리",
+        }}
+      />
+      <Tabs.Screen
+        name="cash"
+        options={{
+          title: "현금",
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ components/order/OrderItem.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+interface OrderItemProps {
+  menuName: string;
+  options: string[];
+  quantity: number;
+  price: string;
+  onIncrease: () => void;
+  onDecrease: () => void;
+  onRemove: () => void;
+}
+
+export default function OrderItem({
+  menuName,
+  options,
+  quantity,
+  price,
+  onIncrease,
+  onDecrease,
+  onRemove,
+}: OrderItemProps) {
+  return (
+    <View className="w-full bg-white rounded-lg p-3 flex flex-row justify-between items-center">
+      {/* 메뉴 이름과 옵션 */}
+      <View className="w-[50%] box-border p-2">
+        <Text className="font-medium text-xl ">{menuName}</Text>
+        <Text className="text-gray-600 text-lg">{options.join(" / ")}</Text>
+      </View>
+      {/* 수량 증가, 감소 */}
+      <View className="w-[25%] flex flex-row items-center justify-around gap-2">
+        <Pressable
+          onPress={onDecrease}
+          className="flex items-center justify-center"
+        >
+          <Ionicons name="remove" size={30} color="black" />
+        </Pressable>
+        <Text className="text-center font-medium text-2xl">{quantity}</Text>
+        <Pressable
+          onPress={onIncrease}
+          className="flex items-center justify-center"
+        >
+          <Ionicons name="add" size={30} color="black" />
+        </Pressable>
+      </View>
+      {/* 가격 */}
+      <View className="w-[25%] flex flex-row items-center justify-center gap-2">
+        <Text className="text-xl font-bold">{price}</Text>
+        <Pressable onPress={onRemove} className="ml-2">
+          <Ionicons name="close" size={24} color="#666" />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
